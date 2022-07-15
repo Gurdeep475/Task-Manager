f5db7c2c65c42ede7ab8d33dbe36eb02d027632b
Gurdeep Singh <gurdeepsingh475475@gmail.com>
2022-07-15 07:57:20 +0530

    updated versions

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const homeRoutes = require("./routes/tasks");
-const MONOCLIENT = require("mongoose");
+const MongoClient = require("mongoose");
 app.use(express.json());
-const MONGO_URI =
-  "mongodb+srv://geekygurdeep:453113@cluster0.tnxo4g8.mongodb.net/tasks?retryWrites=true&w=majority";
+require('dotenv').config();
 app.use("/", homeRoutes);
 // app.use(router,controller);
 
-MONOCLIENT.connect(MONGO_URI)
+MongoClient.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(4000, () => {
       console.log("App have Started Listening");
